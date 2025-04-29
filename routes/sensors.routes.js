b6bfb7cc432334beac4374843be1718d4b82a287
joseAveleira <jose.aveleira@gmail.com>
2025-04-29 16:58:25 +0200

    first commit

---
+++ routes/sensors.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { getAllSensors, createSensor, getSensorById, updateSensor, deleteSensor } = require('../controllers/sensor.controller');
+const authMiddleware = require('../middlewares/auth.Middleware');
+const router = express.Router();
+
+router.get('/', getAllSensors);
+router.post('/',authMiddleware, createSensor);
+router.get('/:id', getSensorById);
+router.put('/:id',authMiddleware, updateSensor);
+router.delete('/:id',authMiddleware, deleteSensor);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+const sensorRoutes = require('./routes/sensors.routes');
+const readingRoutes = require('./routes/readings.routes');
+const authRoutes = require('./routes/auth.routes');
+
+const app = express();
+const port = 3000;
+
+// Conectar a la base de Mongo
+connectDB();
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Rutas dos git commit -m "actualizer readme"
+//senosres y lecturas
+app.use('/sensors', sensorRoutes);
+app.use('/readings', readingRoutes);
+app.use('/auth', authRoutes);
+
+// Ruta de prueba
+app.get('/test', (req, res) => res.json({ msg: 'El API REST funciona!' }));
+
+// Iniciar servidor
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
